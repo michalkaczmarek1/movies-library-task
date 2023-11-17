6fc12758cdf9e04bd0ce12823eeb44128f1a9ac9
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-11-17 18:27:08 +0100

    fixes after review

--- movies_library.py
+++ movies_library.py
@@ -2,7 +2,7 @@ import random
 from datetime import date
 
 
-class Library:
+class Movie():
     def __init__(self, title, year, type, amount_plays):
         self.title = title
         self.year = year
@@ -12,16 +12,11 @@ class Library:
     def play(self):
         return self.amount_plays + 1
 
-
-class Movie(Library):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     def __str__(self):
         return f"{self.title} ({self.year})"
 
 
-class Serie(Library):
+class Serie(Movie):
     def __init__(self, episode_number, season_number, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.episode_number = episode_number
@@ -42,9 +37,10 @@ def get_movies(movies_and_series, content_type):
 def separation_content_type(movies_and_series, content_type):
     elems = []
     for elem in movies_and_series:
-        if isinstance(elem, content_type):
+        if content_type == Movie and hasattr(elem, "episode_number") == False:
             elems.append(elem)
-
+        elif content_type == Serie:
+            elems.append(elem)
     return sorted(elems, key=lambda elem: elem.title)
 
 
@@ -52,7 +48,6 @@ def launcher_function_generate_views(movies_and_series, amount_runs=10):
 
     for i in range(amount_runs):
         generate_views(movies_and_series)
-    return movies_and_series
 
 
 def search_movies_and_series_by_title(movies_and_series, title):
@@ -66,7 +61,6 @@ def generate_views(movies_and_series):
     random_elem = movies_and_series[random.randint(
         0, len(movies_and_series) - 1)]
     random_elem.amount_plays = random.randint(1, 100)
-    return random_elem
 
 
 def top_titles(movies_and_series, amount_chose_movies_and_series, content_type):
@@ -85,22 +79,28 @@ def top_titles(movies_and_series, amount_chose_movies_and_series, content_type):
     if content_type == 1:
         choose_movies = get_movies(movies_and_series, Movie)[
             :amount_chose_movies_and_series]
-        top_titles = sorted(
-            choose_movies, key=lambda elem: elem.amount_plays, reverse=True)
+        top_popular_content = sort_by_amount_plays_desc(choose_movies)
     elif content_type == 2:
         choose_series = get_series(movies_and_series, Serie)[
             :amount_chose_movies_and_series]
-        top_titles = sorted(
-            choose_series, key=lambda elem: elem.amount_plays, reverse=True)
+        top_popular_content = sort_by_amount_plays_desc(choose_series)
 
-    return top_titles
+    return top_popular_content
+
+
+def sort_by_amount_plays_desc(elems_libraries):
+    top_popular_content = sorted(
+        elems_libraries, key=lambda elem: elem.amount_plays, reverse=True)
+    return top_popular_content
 
 
 if __name__ == "__main__":
+
     print("Biblioteka filmów:")
 
     movie_1 = Movie(title="Pulp Fiction", year=1994,
                     type="action", amount_plays=2)
+
     movie_2 = Movie(title="Kac Vegas", year=2009,
                     type="comedy", amount_plays=4)
     movie_3 = Movie(title="Kac Vegas w Bangkoku", year=2011,
@@ -125,7 +125,7 @@ if __name__ == "__main__":
     print(f"Najpopularniejsze filmy i seriale dnia {formatted_date}:")
 
     top_popular_content = top_titles(movies_and_series=movies_and_series,
-                                     amount_chose_movies_and_series=3, content_type=1)
+                                     amount_chose_movies_and_series=3, content_type=2)
 
     for item in top_popular_content:
         print(item)
